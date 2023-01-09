9a7073e581c235e0ad5bd0a399d185272839b911
rajivsuting <rajivsuting16@gmail.com>
2023-01-09 16:16:56 +0530

    push

--- TheoreticalChallenge/src/com/masai/Demo.java
+++ TheoreticalChallenge/src/com/masai/Demo.java
@@ -6,34 +6,67 @@ public class Demo {
 	
 	public static void main(String[]  args) {
 		
-		  String csv = " A1: 5, A2: 7, A3: 9, B1: 3, B2: 8, B3: =4+5, C1: =5+A1, C2: =A2+B2, C3: =C2+B3 ";
+		public static void main(String[] args) {
+			
+			Scanner sc = new Scanner(new File("C:User\\Desktop\\prospecta.csv"));
+			
 
-		  String[] data = csv.split(",");
-		
-		  String[][] table = new String[data.length][data.length];
-		
+//		      String csv = " A1: 5, A2: 7, A3: 9, B1: 3, B2: 8, B3: =4+5, C1: =5+A1, C2: =A2+B2, C3: =C2+B3 ";
 
-		  for (int i = 0; i < data.length; i++) {
-		     table[i] = data[i].split(":");
-		  }
-		
 
-		  for (int i = 0; i < table.length; i++) {
-		     for (int j = 0; j < table[i].length; j++) {
-		        if (table[i][j].contains("=")) {
-		       table[i][j] = table[i][j].replace("=", "");
-		        }
-		     }
-		  }
-		
+//		      String[] data = csv.split(",");
+		      sc.useDelimiter(","); 
 
-		  for (int i = 0; i < table.length; i++) {
-		     for (int j = 0; j < table[i].length; j++) {
-		        System.out.print(table[i][j] + " ");
-		     }
-		     System.out.println();
-		  }
-		
+
+		      Map<String, String> map = new LinkedHashMap<>();
+
+
+		      for (int i = 0; i < data.length; i++) {
+		         String[] pair = data[i].split(":");
+		         map.put(pair[0].trim(), pair[1].trim());
+		      }
+
+
+		      for (Map.Entry<String, String> entry : map.entrySet()) {
+		         if (entry.getValue().startsWith("=")) {
+		            entry.setValue(entry.getValue().substring(1));
+		         }
+		      }
+
+
+		      Map<String, Integer> map2 = new LinkedHashMap<>();
+
+
+		      for (Map.Entry<String, String> entry : map.entrySet()) {
+		         String value = entry.getValue();
+		         if (value.contains("+")) {
+		            String[] operands = value.split("\\+");
+		            int sum = 0;
+
+
+		            for (int i = 0; i < operands.length; i++) {
+		               if (operands[i].matches(".*[A-Z].*")) {
+
+		                  sum += Integer.parseInt(map.get(operands[i]));
+
+		               } else {
+		                  sum += Integer.parseInt(operands[i]);
+		               }
+		            }
+
+		            map.put(entry.getKey(), sum + "");
+		            map2.put(entry.getKey(), sum);
+		         }else{
+		            map2.put(entry.getKey(), Integer.parseInt(value));
+		         }
+		      }
+
+
+		      for (Map.Entry<String, Integer> entry : map2.entrySet()) {
+		         System.out.println(entry.getKey() + " = " + entry.getValue());
+		      }
+
+		   }
 	}
 
 }
